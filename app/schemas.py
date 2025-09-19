939284871d4e75413b801e3f657fe7eea759d73f
Piyolo <135812832+Piyolo@users.noreply.github.com>
2025-09-20 02:21:17 +0800

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -4,15 +4,25 @@ from pydantic import BaseModel
 
 #--------------------------------------
 
+class AccountBase(BaseModel):
+    name: str
+    contact_number: Optional[str] = None
+
+class AccountCreate(AccountBase):
+    password: str  # plain text from client, only for requests
+    
 class AccountIn(BaseModel):
     name: str
     password: str
     contact_number: str | None = None
 
-class AccountOut(BaseModel):
-    name: str
-    password: str
-    contact_number: str | None
+class AccountOut(AccountBase):
+    id: int
+    class Config:
+        # Pydantic v2:
+        from_attributes = True
+        # If you're on Pydantic v1, use:
+        # orm_mode = True
     
 #------------------------------------------
 
@@ -43,9 +53,10 @@ class UserOut(BaseModel):
 
     class Config:
         orm_mode = True  # allows returning SQLAlchemy model objects directly
+        
 class AuthRes(BaseModel):
     token: str
-    user: UserOut
+    user: AccountOut
 
 
 
@@ -63,6 +74,7 @@ class DetectResponse(BaseModel):
     time_ms: float
     detections: List[Detection]
     image_b64: Optional[str] = None  # data:image/jpeg;base64,...
+
 
 
 
