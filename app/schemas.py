999ab815b8cdda21b7cff7179945a19cab964877
Piyolo <135812832+Piyolo@users.noreply.github.com>
2025-09-19 14:47:39 +0800

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -9,9 +9,22 @@ class AccountIn(BaseModel):
     contact_number: str | None = None
 
 class AccountOut(BaseModel):
-    id: int
     name: str
+    password: str
     contact_number: str | None
+
+class SignupIn(BaseModel):
+    name: str
+    password: str
+    contact_number: str | None = None
+
+class LoginIn(BaseModel):
+    name: str
+    password: str
+
+class AuthRes(BaseModel):
+    token: str
+    user: AccountOut
     
 class Box(BaseModel):
     x: float; y: float; w: float; h: float
@@ -26,5 +39,6 @@ class DetectResponse(BaseModel):
     time_ms: float
     detections: List[Detection]
     image_b64: Optional[str] = None  # data:image/jpeg;base64,...
+
 
 
