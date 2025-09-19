33d7647ca212dbe3f4baa5ab694840549b87f695
Piyolo <135812832+Piyolo@users.noreply.github.com>
2025-09-20 00:26:15 +0800

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from typing import List, Optional, Dict, Any
 from pydantic import BaseModel
 
 
+#--------------------------------------
 
 class AccountIn(BaseModel):
     name: str
@@ -12,20 +13,40 @@ class AccountOut(BaseModel):
     name: str
     password: str
     contact_number: str | None
+    
+#------------------------------------------
 
 class SignupIn(BaseModel):
     name: str
     password: str
     contact_number: str | None = None
 
+class SignupReq(AuthReq):
+    contact_number: Optional[str] = None
+
+#-----------------------------------------
+
 class LoginIn(BaseModel):
     name: str
     password: str
+    
+#-----------------------------------------
 
 class AuthRes(BaseModel):
     token: str
-    user: AccountOut
-    
+    user: UserOut
+
+#-----------------------------------------
+
+class UserOut(BaseModel):
+    id: int
+    name: str
+    contact_number: Optional[str] = None
+
+    class Config:
+        orm_mode = True  # allows returning SQLAlchemy model objects directly
+
+#-----------------------------------------
 class Box(BaseModel):
     x: float; y: float; w: float; h: float
 
@@ -39,6 +60,7 @@ class DetectResponse(BaseModel):
     time_ms: float
     detections: List[Detection]
     image_b64: Optional[str] = None  # data:image/jpeg;base64,...
+
 
 
 
